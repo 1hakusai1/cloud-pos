1ff630110c26eb4a4d48795f285d3875151511e6
oishi <oishi@osstech.co.jp>
2022-06-19 06:53:48 +0000

    商品の永続化を担うrepositoryを定義
    インスタンス作成用のメソッドを用意するために、interfaceではなくabstract classとする。ProductのインスタンスはRepositoryの実装でのみ作れるように

---
+++ cloud-pos-core/src/main/java/jp/co/smartware/product/ProductRepository.java
@@ -0,0 +1,21 @@
+package jp.co.smartware.product;
+
+import java.net.URL;
+import java.util.Optional;
+
+public abstract class ProductRepository {
+
+    abstract public Optional<Product> findByJANCode(final String jancode);
+
+    abstract public void update(final Product product) throws ProductRepositoryException;
+
+    protected static Product createInstance(
+            final JANCode janCode,
+            final JapaneseProductName japaneseProductName,
+            final ChineseProductName chineseProductName,
+            final URL imageURL,
+            final int inventoryQuantity) {
+        return new Product(janCode, japaneseProductName, chineseProductName, imageURL, inventoryQuantity);
+    }
+
+}
