09c745d488e3b5449bcf903132a975275102b1b2
oishi <oishi@osstech.co.jp>
2022-06-19 09:04:52 +0000

    在庫不足を計算するクラスの雛形を作成。未実装

---
+++ cloud-pos-core/src/main/java/jp/co/smartware/usecase/OutOfStockDetector.java
@@ -0,0 +1,12 @@
+package jp.co.smartware.usecase;
+
+import jp.co.smartware.order.OrderRepository;
+import jp.co.smartware.product.ProductRepository;
+
+public class OutOfStockDetector {
+
+    public OutOfStockDetector(OrderRepository orderRepository, ProductRepository productRepository) {
+        
+    }
+
+}
