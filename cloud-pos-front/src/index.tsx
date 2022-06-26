4677277e5c88494fae0c27447e3318c5775145e4
1hakusai1 <1hakusai1@gmail.com>
2022-06-26 12:47:38 +0000

    ファイルの置き場所を変更

--- cloud-pos-front/src/index.tsx
+++ cloud-pos-front/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './component/Home';
+import { Home } from './page/Home';
 import { WaitingOrders } from './component/WaitingOrders';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
