70b25c54ec6d854c553ae7c0e5ad29ca19228770
1hakusai1 <1hakusai1@gmail.com>
2022-06-26 12:52:27 +0000

    レイアウトを調整(実験)

--- cloud-pos-front/src/page/Home.tsx
+++ cloud-pos-front/src/page/Home.tsx
@@ -1,11 +1,13 @@
-import { Typography } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
 
 export const Home = () => {
     return (
         <>
-            <Typography variant="h1">Hello</Typography>
-            <Link to={"/waiting"}>wating orders</Link>
+            <Typography variant="h1" textAlign={"center"} sx={{ marginTop: 2 }}>Hello</Typography>
+            <Link to={"/waiting"}>
+                <Typography variant="h6" textAlign={"center"}>wating orders</Typography>
+            </Link>
         </>
     )
 }
