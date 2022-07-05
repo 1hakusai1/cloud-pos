745f78884caa0e0c0266735f9a3713ba6758bde7
1hakusai1 <1hakusai1@gmail.com>
2022-07-05 11:20:20 +0000

    注文情報をインポートするエンドポイントを作成

--- cloud-pos-server/src/main/java/jp/co/smartware/endpoint/OrderResource.java
+++ cloud-pos-server/src/main/java/jp/co/smartware/endpoint/OrderResource.java
@@ -1,21 +1,28 @@
 package jp.co.smartware.endpoint;
 
+import java.io.Reader;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.unchecked.Unchecked;
+import jp.co.smartware.boundary.csv.OrderCSVConverter;
 import jp.co.smartware.dto.OrderDTO;
+import jp.co.smartware.order.LPNumber;
 import jp.co.smartware.order.Order;
 import jp.co.smartware.order.OrderID;
 import jp.co.smartware.order.OrderRepository;
+import jp.co.smartware.order.OrderRepositoryException;
+import jp.co.smartware.product.JANCode;
 
 @Path("/order")
 public class OrderResource {
@@ -44,6 +51,19 @@ public class OrderResource {
                         list -> list.stream()
                                 .map(order -> OrderDTO.fromOrder(order))
                                 .collect(Collectors.toList()));
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Map<String, String>> importCSV(Reader reader) throws OrderRepositoryException {
+        OrderCSVConverter converter = new OrderCSVConverter();
+        List<OrderDTO> dtos = converter.fromCSV(reader);
+        for (OrderDTO dto : dtos) {
+            Map<JANCode, Integer> orderedProducts = dto.orderedProducts.entrySet().stream()
+                    .collect(Collectors.toMap(e -> new JANCode(e.getKey()), e -> e.getValue()));
+            repository.create(new OrderID(dto.orderID), new LPNumber(dto.lpNumber), orderedProducts);
+        }
+        return Uni.createFrom().item(Map.of("message", "ok"));
     }
 
     private OrderDTO convert(Optional<Order> found) {
